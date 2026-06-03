1c338bfefaa9219c26ac1fb66bf292ea6d92090b
Josue Casalins <116575545+JosueRafaelCH@users.noreply.github.com>
2026-06-02 22:28:12 -0500

    Delete src/SimulatorApp directory
    Error

--- src/SimulatorApp/src/main/java/com/example/SimulatorApp/Repository/EstratoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SimulatorApp.Repository;
-
-import com.example.SimulatorApp.Model.Estrato;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstratoRepository extends JpaRepository<Estrato, Long> {
-}
--- src/SimulatorApp/src/main/java/com/example/SimulatorApp/Repository/FeedbackIARepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SimulatorApp.Repository;
-
-import com.example.SimulatorApp.Model.FeedbackIA;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedbackIARepository extends JpaRepository<FeedbackIA, Long> {
-}
